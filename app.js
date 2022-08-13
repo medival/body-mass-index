3c18529488d656c199ec612e706155ef61500aa3
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-08-13 11:16:20 +0700

    add: bmi app (#1)

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mainRoutes = require('./src/routes/main');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'works' });
+});
+
+app.use('/', mainRoutes);
+
+const port = 5000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server started on localhost port ${port}`);
+});
