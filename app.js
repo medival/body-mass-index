c79f51d8791fb5e6a298a2f8338028228b92b43b
medival <dev.adipurnomo@gmail.com>
2022-11-26 17:21:47 +0700

    chore: update response message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.json({ message: 'works' });
+  res.json({ message: 'Implement CICD using Jenkins' });
 });
 
 app.use('/', mainRoutes);
